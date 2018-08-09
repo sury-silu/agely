2f9127034e8b6ffdaef66079456ea6b5638dcc9c
Surya Panda <surya.silu@gmail.com>
2018-08-09 16:38:09 +0530

    product archive: keep the button text to static "readmore" for external product.

--- functions.php
+++ functions.php
@@ -204,7 +204,7 @@ function woocommerce_loop_add_to_cart_link_external_product( $link, $product, $a
 		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
 		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
 		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		esc_html( $product->add_to_cart_text() )
+		'Read More'
 	);
 	
 	return $link;
